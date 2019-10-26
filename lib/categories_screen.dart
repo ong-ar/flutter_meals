c81e0a930bea0f00ba741f7ac3c8319bb6d26a59
ong-ar <reclusis@gmail.com>
2019-10-26 12:26:19 +0900

    카테고리 더미데이터 및 리스트

---
+++ lib/categories_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'category_item.dart';
+
+import 'dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        children: <Widget>[
+          ...DUMMY_CATEGORIES
+              .map((catDate) => CategoryItem(catDate.title, catDate.color))
+        ],
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
