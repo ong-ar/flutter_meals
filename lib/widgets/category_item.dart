d86f91727dccfabcdc1b20f965ad62304346c1d1
ong-ar <reclusis@gmail.com>
2019-10-27 02:38:26 +0900

    onGenerateRoute / onUnknownRoute

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meals/screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
@@ -10,7 +11,7 @@ class CategoryItem extends StatelessWidget {
   // named router
   void selectCategory(BuildContext ctx) {
     Navigator.of(ctx).pushNamed(
-      '/category-meals',
+      CategoryMealsScreen.routeName,
       arguments: {
         'id': id,
         'title': title,
