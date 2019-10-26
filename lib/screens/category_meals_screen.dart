f81e8e7ca5d4f5c85e14450e2a0ea8944f25bd5b
ong-ar <reclusis@gmail.com>
2019-10-27 01:58:54 +0900

    폴더링, meal list

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './dummy-data.dart';
+import 'package:flutter_meals/widgets/meal_item.dart';
+import '../dummy-data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   // final String categoryId;
@@ -24,7 +25,13 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            complexity: categoryMeals[index].complexity,
+            affordability: categoryMeals[index].affordability,
+          );
         },
         itemCount: categoryMeals.length,
       ),
